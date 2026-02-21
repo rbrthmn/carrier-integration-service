c12723e99a2757102ff2d66eccb4cf0ecd686900
rbrthmn <robertokenzohamano@gmail.com>
2026-02-21 19:43:11 -0300

    fix import

--- src/carriers/ups/index.ts
+++ src/carriers/ups/index.ts
@@ -1,5 +1,5 @@
 import { registerCarrier } from '../registry';
 import { UPSCarrier } from './ups-carrier';
-import { loadUPSConfig } from './config';
+import { loadUPSConfig } from './ups-config';
 
 registerCarrier('ups', (http) => new UPSCarrier(loadUPSConfig(), http));
