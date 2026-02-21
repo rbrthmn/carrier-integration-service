07561d8496d52de0ab315da5e44eb1778d44fd37
rbrthmn <robertokenzohamano@gmail.com>
2026-02-21 19:49:01 -0300

    changes axios directory

--- src/http/clients/axios-client.ts
+++ src/http/clients/axios-client.ts
@@ -1,4 +1,4 @@
-import {HttpClient} from "./client";
+import {HttpClient} from "../client";
 
 export class AxiosClient implements HttpClient {
     post<T>(url: string, body: any, options?: { headers?: Record<string, string> }): Promise<T> {
